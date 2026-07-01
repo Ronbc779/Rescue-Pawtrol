be55506a660f5c07077c8c4d520f1c520cc0dc08
Ron <rbcabungcag@up.edu.ph>
2026-07-01 20:02:56 +0800

    feat: repositioned shield

--- src/scenes/Level2.js
+++ src/scenes/Level2.js
@@ -23,12 +23,24 @@ class Level2 extends Phaser.Scene {
     create(){
         const CATS = ["white_cat1", "white_cat2", "gray_cat1", "gray_cat2", "orange_cat1", "orange_cat2"]
         const random = Phaser.Utils.Array.GetRandom(CATS);
+        const CX = this.scale.width / 2;
+        const CY = this.scale.height / 2;
         this.cat = this.physics.add.staticSprite(
-            this.scale.width / 2, 
-            this.scale.height / 2, 
+            CX, 
+            CY, 
             random
         );
-        this.shield = this.add.rectangle(0, 0, 50, 14, 0x66ccff);
+        this.cat.body.setCircle(20);
+        
+        this.shield = this.add.rectangle(
+            CX, CY - 70,
+            this.shieldWidth, this.shieldHeight,
+            0x66ccff, 0.9
+        );
+
+        this.shield.setStrokeStyle(2, 0xffffff, 1);
+        this.physics.add.existing(this.shield, false);
+        this.shield.body.setImmovable(true);
 
     }
     update(){
