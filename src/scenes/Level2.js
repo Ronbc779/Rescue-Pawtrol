8c3964eaa7e24c2265645cf661fff67f64f482c7
Ron <rbcabungcag@up.edu.ph>
2026-07-01 17:43:47 +0800

    add: initial Level2 added

--- src/scenes/Level2.js
+++ src/scenes/Level2.js
@@ -3,9 +3,17 @@ class Level2 extends Phaser.Scene {
         super({ key: 'Level2' });
     }
     create(){
+        const CATS = ["white_cat1", "white_cat2", "gray_cat1", "gray_cat2", "orange_cat1", "orange_cat2"]
+        const random = Phaser.Utils.Array.GetRandom(CATS);
+        this.cat = this.physics.add.staticSprite(
+            this.scale.width / 2, 
+            this.scale.height / 2, 
+            random
+        );
+        this.shield = this.add.rectangle(0, 0, 50, 14, 0x66ccff);
 
     }
     update(){
-        
+        const POINTER = this.input.activePointer;
     }
 }
